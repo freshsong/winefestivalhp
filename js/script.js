2e0a9300b9ab32f7e915871932b61928243f7c30
신선한 노래 <racimo.715@gmail.com>
2024-06-19 09:08:18 +0900

    123

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+   // 마우스오버 메뉴
+     $('.navi > li').mouseover(function(){
+       $(this).find('.subMenu').stop().slideDown(300);
+     }).mouseout(function(){
+       $(this).find('.subMenu').stop().slideUp(300);
+     });
+   
+   
+   
+   });
+
+   
